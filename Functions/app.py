f63399cfc18963bc08db3cbc7ac821a27834c997
Leonardo Miguel <leonardomiguelvs84@gmail.com>
2024-11-05 17:11:40 -0300

    Editar Receita

--- Functions/app.py
+++ Functions/app.py
@@ -80,7 +80,7 @@ def home():
         try:
             query = "SELECT * FROM laricabook.receitas"
             receitas = session_bd.execute(query)
-
+            print("testes", receitas)
             extensoes_permitidas = ['jpg', 'png', 'jpeg']
             receitas_list_home = []
             
@@ -97,6 +97,7 @@ def home():
 
                 
                 receitas_list_home.append({
+                    'id_receita': receita.id_receita,
                     'nome_receita': receita.nome_receita,
                     'categoria': receita.categoria,
                     'fonte_link': receita.fonte_links,  
@@ -129,30 +130,25 @@ def logOff():
 @app.route('/HomeReceita')
 def homeReceita():
     if session.get('nome_usuario') is None :
-        return render_template('index.html')
-        print(f"tem sessão: " + session['nome_usuario'])
+        return render_template('index.html')        
     else:
         receitas_list = []
         try:
             query = "SELECT * FROM laricabook.receitas WHERE receita_publica = true ALLOW FILTERING"
-            receitas = session_bd.execute(query)
-            print(receitas)
-            
+            receitas = session_bd.execute(query)           
             extensoes_permitidas = ['jpg', 'png', 'jpeg', 'gif']
             for receita in receitas:
                 
                 caminho_imagem = None
                 for ext in extensoes_permitidas:
                     nome_imagem = f"{receita.id_usuario}_{receita.id_receita}.{ext}"
-                    caminho_possivel = os.path.join("static/Imagens", nome_imagem)
+                    caminho_possivel = os.path.join(UPLOAD_FOLDER, nome_imagem)
                     if os.path.exists(caminho_possivel):  # Verifica se o arquivo existe
-                        caminho_imagem = caminho_possivel
+                        caminho_imagem = f"Imagens/{nome_imagem}"
                         break  # Para o loop quando encontrar uma imagem
             
-
-            for receita in receitas:
-                print(receita)
                 receitas_list.append({
+                    'id_receita': receita.id_receita,
                     'nome_receita': receita.nome_receita,
                     'categoria': receita.categoria,
                     'fonte_link': receita.fonte_links,  
@@ -208,6 +204,67 @@ def add_receita():
         print(f"Erro ao inserir a receita: {e}")
 
     return render_template('InsereReceita.html')
+
+@app.route('/EditarReceita/<uuid:id_receita>')
+def edit_receita(id_receita):
+    if session.get('nome_usuario') is None :
+        return render_template('index.html')        
+    else:        
+        try:
+            extensoes_permitidas = ['jpg', 'png', 'jpeg', 'gif']            
+            result = session_bd.execute(
+                "SELECT * FROM laricabook.receitas WHERE id_receita=%s", (id_receita,)
+            )
+            receita = result.one() 
+            list_receita = []
+            list_receita.append(receita)                                 
+            caminho_imagem = None
+            for ext in extensoes_permitidas:
+                nome_imagem = f"{receita.id_usuario}_{receita.id_receita}.{ext}"
+                caminho_possivel = os.path.join(UPLOAD_FOLDER, nome_imagem)
+                if os.path.exists(caminho_possivel):  # Verifica se o arquivo existe
+                    caminho_imagem = f"Imagens/{nome_imagem}"
+                    list_receita.append({ 
+                        'imagem': caminho_imagem
+                    })
+                    print(list_receita)
+                    break  # Para o loop quando encontrar uma imagem
+            
+            if receita:
+                return render_template('EditarReceita.html', receita=list_receita)
+            else:
+                print("Receita não encontrada")
+                return redirect(url_for('insereReceita'))
+        except Exception as e:
+            print(f"Erro ao carregar receita para edição: {e}")
+            return redirect(url_for('home'))
+            
+@app.route('/AtualizarReceita', methods=['POST'])
+def atualizar_receita():
+    id_receita2 = request.form['id_receita']
+    nome_receita = request.form['nome_receita']  # Mantém o mesmo nome
+    categoria_receita = request.form['categoria']
+    fontes_link_receita = request.form['fontes_links']
+    ingredientes_receita = request.form['ingredientes']
+    ingredientes_lista = [ingrediente.strip() for ingrediente in ingredientes_receita.split(',')]
+    instrucoes_receita = request.form['instrucoes']
+    porcoes_receita = int(request.form['porcoes'])
+    tempo_preparo_receita = int(request.form['tempo_preparo'])
+    receita_publica = request.form['publico'] == 'true'
+
+    try:
+        id_receita = uuid.UUID(id_receita2)
+        session_bd.execute(""" 
+        UPDATE laricabook.receitas 
+        SET categoria = %s, fonte_links = %s, ingredientes = %s, instrucoes = %s, porcoes = %s, tempo_preparo = %s, receita_publica = %s
+        WHERE id_receita = %s AND nome_receita = %s
+        """, (categoria_receita, fontes_link_receita, ingredientes_lista, instrucoes_receita, porcoes_receita, tempo_preparo_receita, receita_publica, id_receita, nome_receita))
+
+        return render_template('Home')
+
+    except Exception as e:
+        print(f"Erro ao atualizar a receita: {e}")
+        return render_template('InsereReceita.html', error="Erro ao atualizar a receita.")
 
 
 
